dd2045691da0d3155ffa479118d9de964dd9f80e
bbarker <beb82@cornell.edu>
2016-03-31 16:17:53 -0400

    minor cleanup

--- src/main/java/edu/ncrn/cornell/model/dao/BaseRepository.java
+++ src/main/java/edu/ncrn/cornell/model/dao/BaseRepository.java
@@ -2,16 +2,14 @@ package edu.ncrn.cornell.model.dao;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
-import org.springframework.data.repository.Repository;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
  
 @NoRepositoryBean
 interface BaseRepository<T, ID extends Serializable> extends CrudRepository<T, ID> {
 
-    public List<T> findAll();
+    List<T> findAll();
     //TODO: for impls,  possible canonical implementation (remove when implemented):
     //    List<T> findAll() {
     //        Iterable<T> all = findAll();
