5bb009e0ef8a79dbc5de1f6e3eea222793c36f86
bbarker <beb82@cornell.edu>
2016-03-31 13:31:52 -0400

    minor: switch from Repository to CrudRepository

--- src/main/java/edu/ncrn/cornell/model/dao/BaseRepository.java
+++ src/main/java/edu/ncrn/cornell/model/dao/BaseRepository.java
@@ -1,21 +1,23 @@
 package edu.ncrn.cornell.model.dao;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.Repository;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
  
 @NoRepositoryBean
-interface BaseRepository<T, ID extends Serializable> extends Repository<T, ID> {
- 
-    void delete(T deleted);
- 
-    List<T> findAll();
+interface BaseRepository<T, ID extends Serializable> extends CrudRepository<T, ID> {
+
+    public List<T> findAll();
+    //TODO: for impls,  possible canonical implementation (remove when implemented):
+    //    List<T> findAll() {
+    //        Iterable<T> all = findAll();
+    //        List<T> target = new ArrayList<>();
+    //        all.forEach(target::add);
+    //        return target;
+    //    };
      
-    T findOne(ID id);
- 
-    T save(T persisted);
-    
-    long count();
 }
