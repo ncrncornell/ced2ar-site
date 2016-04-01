d126f6daff59ecf6670ab7492478692a88794162
Kyle Brumsted <kyle.brumsted@mail.mcgill.ca>
2016-04-01 09:44:40 -0400

    empty files for xml upload

---
+++ src/main/java/edu/ncrn/cornell/service/UploadService.java
@@ -0,0 +1,12 @@
+package edu.ncrn.cornell.service;
+
+/**
+ * Class to handle the upload mechanisms of new codebooks:
+ * Upon upload, cascade new data into the field_inst and field indices tables
+ * Also check for new schemas.
+ * @author kylebrumsted
+ *
+ */
+public class UploadService {
+
+}
---
+++ src/main/java/edu/ncrn/cornell/web/UploadController.java
@@ -0,0 +1,14 @@
+package edu.ncrn.cornell.web;
+
+/**
+ * Controller class to handle the upload interface for new codebooks.
+ * @author kylebrumsted
+ *
+ */
+public class UploadController {
+	
+	//GET page
+	
+	//PUT page
+
+}
