2862e49ef2a39b5ad3c94bca4d3c5d55a4bb485a
Kyle Brumsted <kyle.brumsted@mail.mcgill.ca>
2016-04-04 09:57:58 -0400

    [CDR-58] comment cleanup

--- src/main/java/edu/ncrn/cornell/model/dao/BaseRepository.java
+++ src/main/java/edu/ncrn/cornell/model/dao/BaseRepository.java
@@ -10,12 +10,5 @@ import java.util.List;
 interface BaseRepository<T, ID extends Serializable> extends CrudRepository<T, ID> {
 
     List<T> findAll();
-    //TODO: for impls,  possible canonical implementation (remove when implemented):
-    //    List<T> findAll() {
-    //        Iterable<T> all = findAll();
-    //        List<T> target = new ArrayList<>();
-    //        all.forEach(target::add);
-    //        return target;
-    //    };
-     
+
 }
