d6a8fa8f27cedc211eefac0955dfd64fb7ffe3a7
bbarker <beb82@cornell.edu>
2016-05-26 17:44:36 -0400

    [CDR-105] Attempting to move spring app test to integration

--- src/test/java/edu/ncrn/cornell/Ced2arApplicationIT.java
+++ src/test/java/edu/ncrn/cornell/Ced2arApplicationIT.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Ced2arApplication.class)
 @WebAppConfiguration
-public class Ced2arApplicationTests {
+public class Ced2arApplicationIT {
 
 	@Test
 	public void contextLoads() {
