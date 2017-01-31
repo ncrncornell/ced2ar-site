a924bacf59820a1e1ceceb4636a1758e9cf57cdb
Brandon Elam Barker <beb82@cornell.edu>
2017-01-31 14:18:07 -0500

    improving view of data files

--- site/src/main/java/edu/ncrn/cornell/site/view/CodebookView.scala
+++ site/src/main/java/edu/ncrn/cornell/site/view/CodebookView.scala
@@ -3,47 +3,64 @@ package edu.ncrn.cornell.site.view
 import edu.ncrn.cornell.site.view.common.Ced2arView
 import org.springframework.stereotype.Component
 
+import scalatags.Text.TypedTag
 import scalatags.Text.all._
 
 
 @Component
 class CodebookView extends Ced2arView {
 
+  val collapsableFields = Set("Files")
+
   def codebookDetails(
      details : Map[(String, Int), String],
      handle : String
   ) : String = {
-      val detailsSorted = details.toSeq.sortBy(_._1._2)
-      val typedHtml = html(
-        head(
-          defaultMetaTags,
-          defaultStyleSheetsAndScripts
-        ),
-        body(masterDiv(
+    val detailsSorted = details.toSeq.sortBy(_._1._2)
+    val typedHtml = html(
+      head(
+        defaultMetaTags,
+        defaultStyleSheetsAndScripts
+      ),
+      body(masterDiv(
         topBanner,
         navBar,
         ol(cls:="breadcrumb")(
           li(a(href:= s"$servletPath/codebooks")("Codebooks")),
           li(cls:="active")(handle)
         ),
-        masterDiv(
-            a(href := s"$servletPath/codebooks/$handle/vars")
+        div(
+          a(href := s"$servletPath/codebooks/$handle/vars")
          ("View Variables")
-         ),
-         masterDiv(
+        ),
+        div(
           detailsSorted.map{case ((fieldName, order), fieldValue) =>
-            div(
-              h3(fieldName),
-              p(fieldValue)
-            )
+            renderField(fieldName, fieldValue)
           }
         )
-         )
-       )
+      ))
+    )
+    typedHtml.toString()
+  }
+
+  private def renderField(fieldName: String, fieldValue: String): TypedTag[String] =
+    if (collapsableFields.contains(fieldName))
+      div(
+        h3(
+          a(`class` := "glyphicon glyphicon-menu-down",
+            href := s"#$fieldName-detail",
+            attr("data-toggle") := "collapse",
+            fieldName
+          )
+        ),
+        p(id := s"$fieldName-detail", `class` := "collapse",
+          fieldValue
+        )
       )
-      typedHtml.toString()
-      
-   }
-  
+    else
+      div(
+        h3(fieldName),
+        p(fieldValue)
+      )
   
 }
