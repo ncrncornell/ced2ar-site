85fb91b12ce37451808f46c76371913b24b9e06a
Brandon Elam Barker <beb82@cornell.edu>
2017-02-15 16:41:12 -0500

    CDR-184: added initial JSON support and tests

--- site/src/test/java/edu/ncrn/cornell/model/Ced2arRepositoryIT.scala
+++ site/src/test/java/edu/ncrn/cornell/model/Ced2arRepositoryIT.scala
@@ -2,10 +2,17 @@ package edu.ncrn.cornell.model
 
 import edu.ncrn.cornell.Ced2arApplication
 import edu.ncrn.cornell.model.testing.{BaseRepositoryIT, DBChecker}
+import edu.ncrn.cornell.service.CodebookService
+import org.junit.Test
+//import org.junit.gen5.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.TestExecutionListeners
+
+
+import io.circe._
+import io.circe.parser._
 
 /**
   * @author Brandon Elam Barker
@@ -16,7 +23,26 @@ import org.springframework.test.context.TestExecutionListeners
 //@DatabaseSetup(BaseRepositoryIT.DATASET)
 //@DatabaseTearDown(type = DatabaseOperation.DELETE_ALL, value = { BaseRepositoryIT.DATASET })
 //@DirtiesContext
-class Ced2arRepositoryIT extends BaseRepositoryIT {
+class Ced2arRepositoryIT extends BaseRepositoryIT  {
+
+  //TODO: refactor to services-core-tests module
+
+  @Autowired val codeBookService: CodebookService = null
+
+  @Test
+  def getAllHandlesIsJson = {
+    val jsonHandles = codeBookService.getAllHandlesJson
+    val parseResult = parse(jsonHandles)
+    val jsonIsNonTrivial = parseResult match {
+      case Right(someJson) =>
+        println(someJson.toString()) //DEBUG
+        someJson.toString().length > 3
+      case Left(failure) =>
+        println(failure.toString)
+        false
+    }
+    assert(jsonIsNonTrivial)
+  }
 
 
 
