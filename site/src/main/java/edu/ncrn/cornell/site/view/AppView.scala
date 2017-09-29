2c328af4d95ee1980e14dcb5d2f36dd18c0098ed
Brandon Elam Barker <beb82@cornell.edu>
2017-09-29 18:38:38 +0000

    cleaning up js/css/font building/packaging

--- site/src/main/java/edu/ncrn/cornell/site/view/AppView.scala
+++ site/src/main/java/edu/ncrn/cornell/site/view/AppView.scala
@@ -2,33 +2,11 @@ package edu.ncrn.cornell.site.view
 
 import edu.ncrn.cornell.site.view.common.Ced2arView
 import org.springframework.stereotype.Component
-
-//import scalatags.Text.TypedTag
-//import scalatags.Text.all._
-
+import AppView._
 
 @Component
 class AppView extends Ced2arView {
 
-//  lazy val appContainer: String = {
-//    val typedHtml = html(
-//      head(
-//        defaultMetaTags,
-//        defaultStyleSheetsAndScripts
-//      ),
-//      body(
-//        masterDiv(
-//          div(id := "application-container"),
-//          script(
-//            `type` := "application/javascript",
-//            src := //FIXME: config to use fullopt from pom.
-//            s"$servletPath/scalajs-bundler/main/ced2ar3-view-fastopt-bundle.js"
-//          )
-//        )
-//      )
-//    )
-//    typedHtml.toString()
-//  }
 lazy val appContainer: String =
   s"""
     |<!DOCTYPE html>
@@ -41,10 +19,18 @@ lazy val appContainer: String =
     |<body>
     |  <div id="application-container"></dib>
     |  <script type="application/javascript"
-    |    src="$servletPath/scalajs-bundler/main/ced2ar3-view-fastopt-bundle.js"
+    |    src="$servletPath/target/scala-$scalaMajorVer/scalajs-bundler/main/ced2ar3-view-fastopt-bundle.js"
     |  ></script>
     |</body>
     |</html>
   """.stripMargin
   
+}
+
+object AppView {
+
+  protected val rmMinorVer = "\\.[0-9]*$"
+  protected val scalaMajorVer = scala.util.Properties.scalaPropOrElse("version.number", "unknown")
+    .replaceFirst(rmMinorVer, "")
+
 }
